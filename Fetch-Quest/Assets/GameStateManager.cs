f7b6a2475905fe832943a6e0f2a9737f32222d31
Daniel Lin <dlin@scu.edu>
2024-05-19 16:18:22 -0700

    Rearranged Files

---
+++ Fetch-Quest/Assets/GameStateManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum GameState
+    {
+        Intro,
+        Playing,
+        Failure,
+        Win
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
