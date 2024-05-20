f68164c6bfdd66f6f7fb34b004865ca66c968b5e
Daniel Lin <dlin@scu.edu>
2024-05-19 20:37:17 -0700

    Game updates
    Added prefabs for the scrolling buildings

---
+++ Fetch-Quest/Assets/BuildingMover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject lastBuilding;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lastBuilding.transform.position.z <= 73)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // when the gamestate is playing, move towards the camera, after the last building passes by the camera kill yourself
+    // create an empty game object on the end of the prefab and check the position of it
+}
--- Fetch-Quest/Assets/GameStateManager.cs
+++ Fetch-Quest/Assets/GameStateManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,11 @@ using UnityEngine;
 public class GameStateManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static GameStateManager Instance;
+    public GameState currentGameState;
+
+    public static event Action<GameState> gameStateChanged;
+
     public enum GameState
     {
         Intro,
@@ -13,9 +19,31 @@ public class GameStateManager : MonoBehaviour
         Win
     }
 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     void Start()
     {
-        
+        ChangeGameState(GameState.Intro);
+    }
+
+    public void ChangeGameState(GameState targetGameState)
+    {
+        currentGameState = targetGameState;
+        switch (targetGameState)
+        {
+            case GameState.Intro:
+                break;
+            case GameState.Playing:
+                break;
+            case GameState.Failure:
+                break;
+            case GameState.Win:
+                break;
+        }
+        gameStateChanged?.Invoke(targetGameState);
     }
 
     // Update is called once per frame
