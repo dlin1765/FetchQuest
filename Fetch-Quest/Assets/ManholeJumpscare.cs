543884bf2fbb8b1924383046423d6fdac7fb1f8b
justinlee790b <justinlee790b@gmail.com>
2024-05-20 20:05:24 -0700

    fixed bugs with manhole animations.

--- Fetch-Quest/Assets/ManholeJumpscare.cs
+++ Fetch-Quest/Assets/ManholeJumpscare.cs
@@ -5,23 +5,49 @@ using UnityEngine;
 public class ManholeJumpscare : MonoBehaviour
 {
     [SerializeField] private GameObject RobotModel;
-    // Start is called before the first frame update
+    private Transform originalParent;
+    private Vector3 originalPosition;
+    private Rigidbody robotRigidbody;
+
+  
     void Start()
     {
+        originalParent = RobotModel.transform.parent; 
+        originalPosition = RobotModel.transform.localPosition; 
+        robotRigidbody = RobotModel.GetComponent<Rigidbody>();
+        if (robotRigidbody != null)
+        {
+            robotRigidbody.isKinematic = true;
+        }
         RobotModel.SetActive(false);
         StartCoroutine(StartJumpScare());
     }
-    
-    // Update is called once per frame
 
-    void Update()
-    {
-        
-    }
     private IEnumerator StartJumpScare()
     {
         yield return new WaitForSeconds(3);
         RobotModel.SetActive(true);
-    } 
+        RotateManhole();
+    }
 
+    private void RotateManhole()
+    {
+        if (robotRigidbody != null)
+        {
+            robotRigidbody.isKinematic = true; 
+        }
+
+        Vector3 worldPosition = RobotModel.transform.position; 
+
+        RobotModel.transform.SetParent(null);
+        transform.Rotate(new Vector3(90, 0, 0)); 
+
+        RobotModel.transform.position = worldPosition; 
+        RobotModel.transform.SetParent(originalParent); 
+
+        if (robotRigidbody != null)
+        {
+            robotRigidbody.isKinematic = false; 
+        }
+    }
 }
